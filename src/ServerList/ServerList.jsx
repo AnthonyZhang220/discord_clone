1277eccd42e072e4862b2e7781c8024a45e6331b
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2023-03-10 23:05:21 -0500

    discord layout almost done

--- src/ServerList/ServerList.jsx
+++ src/ServerList/ServerList.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
+import { Stack } from '@mui/system'
+import { Avatar } from '@mui/material'
+import { Box } from '@mui/system'
+import "./ServerList.scss"
 
 const ServerList = () => {
     return (
-        <div className='serverlist-container'>
-            <Stack direction="column" spacing={2}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-                <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-            </Stack>
-        </div>
+        <Box component='aside' className="serverlist-container">
+            <Box component="ul" className="serverlist-tree" role="tree">
+                <Box className="serverlist-scroller">
+                    <Box className="serverlist-items">
+                        <Box className="listItem">
+                            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                        </Box>
+                        <Box className="listItem">
+                            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                        </Box>
+                        <Box className="listItem">
+                            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                        </Box>
+                        <Box className="listItem">
+                            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                        </Box>
+                        <Box className="listItem">
+                            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                        </Box>
+                        <Box className="listItem">
+                            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                        </Box>
+                        <Box className="listItem">
+                            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                        </Box>
+                        <Box className="listItem">
+                            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                        </Box>
+                        <Box className="listItem">
+                            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
