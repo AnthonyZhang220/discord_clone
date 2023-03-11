23bf5f2bf453e24ff689f2c9327b7e0d9aceaaaf
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2023-03-11 12:09:06 -0500

    font and layout done

--- src/App.js
+++ src/App.js
@@ -3,19 +3,41 @@ import Chat from './Chat/Chat'
 import ServerList from './ServerList/ServerList'
 import Channel from './Channel/Channel'
 import { Box } from '@mui/system'
+import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material'
+
 
 import "./App.scss";
 
 
+let theme = createTheme({
+    typography: {
+        fontFamily: ["gg sans", "gg sans Normal", "gg sans Medium", "gg sans SemiBold", "sans-serif"].join(","),
+        fontSize: 14,
+        h3: {
+            color: "#fff",
+        },
+        h5: {
+            color: "#fff",
+        },
+        p: {
+            color: "#fff",
+        }
+    },
+});
+theme = responsiveFontSizes(theme);
+
 const App = () => {
     return (
-        <Box className="app-mount">
-            <Box className="app-container">
-                <ServerList />
-                <Channel />
-                <Chat />
+        <ThemeProvider theme={theme}>
+            {/* <CssBaseline /> */}
+            <Box className="app-mount">
+                <Box className="app-container" >
+                    <ServerList />
+                    <Channel />
+                    <Chat />
+                </Box>
             </Box>
-        </Box>
+        </ThemeProvider>
 
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,10 @@ module.exports = {
                     'sass-loader',
                 ],
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
     plugins: [
