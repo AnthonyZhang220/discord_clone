2ecb49eb76e19e2d4cccfab719bf50cd63bd3a29
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2023-04-02 03:10:40 -0400

    add functions to display image video and audio in chat message and fix UI bugs

---
+++ src/StatusList.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+import CircleIcon from '@mui/icons-material/Circle';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import StopCircleIcon from '@mui/icons-material/StopCircle';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+
+
+const StatusList = ({ status, size, edge }) => {
+    const list = {
+        "online": <CircleIcon edge={edge} sx={{ color: "#23a55a", fontSize: size }} />,
+        "idle": <DarkModeIcon edge={edge} sx={{ color: "#f0b132", fontSize: size }} />,
+        "donotdisturb": <RemoveCircleIcon edge={edge} sx={{ color: "#f23f43", fontSize: size }} />,
+        "invisible": <StopCircleIcon edge={edge} sx={{ color: "#80848e", fontSize: size }} />,
+    }
+    return (
+        list[status]
+    )
+}
+
+export default StatusList;
