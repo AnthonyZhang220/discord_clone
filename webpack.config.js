1517e04a6f0e9af78299b7f76b552b0744709792
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2023-04-19 21:33:30 -0400

    ready to deploy on firebase

--- webpack.config.js
+++ webpack.config.js
@@ -59,16 +59,5 @@ module.exports = {
             template: path.join(__dirname, 'public', 'index.html')
         }),
         new DotenvWebpackPlugin(),
-        // new EnvironmentPlugin(['REACT_APP_FIREBASE_API_KEY', 'REACT_APP_AUTH_DOMAIN', "REACT_APP_DATABASE_URL", "REACT_APP_PROJECT_ID", "REACT_APP_STORAGE_BUCKET", "REACT_APP_MESSAGING_SENDER_ID", "REACT_APP_FIREBASE_APP_ID", "REACT_APP_MEASUREMENT_ID"]),
-        // new DefinePlugin({
-        //     'process.env.REACT_APP_FIREBASE_API_KEY': JSON.stringify(process.env.REACT_APP_FIREBASE_API_KEY),
-        //     'process.env.REACT_APP_AUTH_DOMAIN': JSON.stringify(process.env.REACT_APP_AUTH_DOMAIN),
-        //     'process.env.REACT_APP_DATABASE_URL': JSON.stringify(process.env.REACT_APP_REACT_APP_DATABASE_URL),
-        //     'process.env.REACT_APP_PROJECT_ID': JSON.stringify(process.env.REACT_APP_REACT_APP_PROJECT_ID),
-        //     'process.env.REACT_APP_STORAGE_BUCKET': JSON.stringify(process.env.REACT_APP_STORAGE_BUCKET),
-        //     'process.env.REACT_APP_MESSAGING_SENDER_ID': JSON.stringify(process.env.REACT_APP_MESSAGING_SENDER_ID),
-        //     'process.env.REACT_APP_FIREBASE_APP_ID': JSON.stringify(process.env.REACT_APP_FIREBASE_APP_ID),
-        //     'process.env.REACT_APP_MEASUREMENT_ID': JSON.stringify(process.env.REACT_APP_MEASUREMENT_ID),
-        // }),
     ]
 };
