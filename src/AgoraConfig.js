e2d1d2dfed4e5fba3f19e1527ec74b83e4fc25a4
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2023-12-31 16:50:40 -0500

    remove duplicate file from last commit

--- src/AgoraConfig.js
+++
@@ -1,5 +0,0 @@
-export const rtcProps = {
-    appId: process.env.REACT_APP_AGORA_APP_ID,
-    channel: 'test', // your agora channel
-    token: null // use null or skip if using app in testing mode
-};
