7b05029d6393b757d497de2affdd4757f9fb3539
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2024-01-09 03:05:58 -0500

    dotenv webpack

--- src/utils/handlers/searchHandlers.js
+++ src/utils/handlers/searchHandlers.js
@@ -4,10 +4,11 @@ import store from "../../redux/store";
 import { setQueryFriendList } from "../../redux/features/directMessageSlice";
 
 export const handleSearchFriend = async (e) => {
+    e.preventDefault();
+    let list = []
     const searchTerm = e.target.value;
     const userRef = collection(db, "users")
     const q = query(userRef, or(where("id", "==", searchTerm), where("displayName", ">=", searchTerm), where("displayName", "<=", searchTerm + "~")))
-    let list = []
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
         const data = doc.data();
