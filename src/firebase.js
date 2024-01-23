89cd06b91f953fcc5fac508d3f10d31d631c4372
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2024-01-23 01:54:05 -0500

    change build process in github action

--- src/firebase.js
+++ src/firebase.js
@@ -8,19 +8,19 @@ import { getDatabase } from "firebase/database"
 
 
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';0
+import { getFirestore } from 'firebase/firestore'; 0
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+    apiKey: String(process.env.REACT_APP_FIREBASE_API_KEY),
+    authDomain: String(process.env.REACT_APP_AUTH_DOMAIN),
+    databaseURL: String(process.env.REACT_APP_DATABASE_URL),
+    projectId: String(process.env.REACT_APP_PROJECT_ID),
+    storageBucket: String(process.env.REACT_APP_STORAGE_BUCKET),
+    messagingSenderId: String(process.env.REACT_APP_MESSAGING_SENDER_ID),
+    appId: String(process.env.REACT_APP_FIREBASE_APP_ID),
+    measurementId: String(process.env.REACT_APP_MEASUREMENT_ID),
 };
 
 // Initialize Firebase
