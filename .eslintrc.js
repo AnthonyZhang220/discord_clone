205a3c7f0de108632c0aa864a1fa2ef24cf9bd0a
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2026-05-25 20:55:41 -0400

    chore: add lint/prettier/editorconfig matching project style

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier', 'unused-imports'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'react/prop-types': 'off',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+    ]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
